2763d42b16a171d32704fa8d46dd60c3beefb2fa
Karimah Azzuhdu T.M <43397349+KarimahAzzuhdu@users.noreply.github.com>
2024-10-16 10:25:16 +0700

    add new test

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,25 +1,60 @@
 import re
+import pytest
 from playwright.sync_api import Page, expect
 
-def test_login_process(page: Page) -> None:
-    page.goto("https://www.saucedemo.com/")
-
-    page.locator("[data-test=\"username\"]").click()
-    page.locator("[data-test=\"username\"]").fill("standard_user")
-    page.locator("[data-test=\"password\"]").click()
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
-    page.locator("[data-test=\"login-button\"]").click()
-
-    expect(page.locator("[data-test=\"inventory-container\"]")).to_be_visible()
-
-def test_logout_process(page: Page) -> None:
-    page.goto("https://www.saucedemo.com/")
-
-    page.locator("[data-test=\"username\"]").fill("standard_user")
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
-    page.locator("[data-test=\"login-button\"]").click()
-
-    page.get_by_role("button", name="Open Menu").click()
-    page.locator("[data-test=\"logout-sidebar-link\"]").click()
+@pytest.fixture(scope="function", autouse=True)
+def before_each_after_each(page: Page):
     
-    expect(page.locator("[data-test=\"login-button\"]")).to_be_visible()
+    #before each
+    page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    yield
+
+    #after each
+
+def test_login_process(page: Page):
+    #Check Login Page's Attibutes
+    expect(page.get_by_role("heading", name="Login")).to_be_visible()
+    expect(page.get_by_placeholder("Username")).to_be_visible()
+    expect(page.get_by_placeholder("Password")).to_be_visible()
+    expect(page.get_by_role("button", name="Login")).to_be_visible()
+
+    #Login Process
+    page.get_by_placeholder("Username").click()
+    page.get_by_placeholder("Username").fill("Admin")
+    page.get_by_placeholder("Password").click()
+    page.get_by_placeholder("Password").fill("admin123")
+    page.get_by_role("button", name="Login").click()
+
+    #Check user successfully logs in
+    expect(page.get_by_role("heading", name="Dashboard")).to_be_visible()
+
+def test_logout_process(page: Page):
+    #Prerequisites : User already logs in
+    page.get_by_placeholder("Username").fill("Admin")
+    page.get_by_placeholder("Password").fill("admin123")
+    page.get_by_role("button", name="Login").click()
+
+    #Logout Process
+    page.locator('.oxd-userdropdown-tab').click()
+    page.get_by_role("menuitem", name="Logout").click()
+
+    #Check user successfully logs out
+    expect(page.get_by_role("heading", name="Login")).to_be_visible()
+
+def test_invalid_creds(page: Page):
+    #Login with invalid creds Process
+    page.get_by_placeholder("Username").fill("Invalid")
+    page.get_by_placeholder("Password").fill("invalid")
+    page.get_by_role("button", name="Login").click()
+
+    #Check invalid creds alert message
+    expect(page.get_by_role("alert")).to_be_visible()
+
+def test_blank_field(page: Page):
+    #Login with blank field Process
+    page.get_by_role("button", name="Login").click()
+
+    #Check required message
+    expect(page.get_by_text("UsernameRequired")).to_be_visible()
+    expect(page.get_by_text("PasswordRequired")).to_be_visible()
